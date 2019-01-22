3a9349f51c2cdc6bc2317fa6adb40750211debd6
mkobar <mike@exlorev.com>
2019-01-22 11:52:00 -0500

    added tracing

--- iSearchWsApi/blueprints/api/raw.py
+++ iSearchWsApi/blueprints/api/raw.py
@@ -39,8 +39,22 @@ def googleRaw():
       else:
         return ('{"message": "mocked"}')
     #if (mock == 0):
-    res = requests.get('https://google.com/search?q=' +q+ '&oq='+q+'&hl=en&gl=us&sourceid=chrome&ie=UTF-8')
-    res.raise_for_status()
+    if (q == '500'):
+      # 503 Server Error: Service Unavailable for url: https://www.google.com/sorry/index?continue=https://www.google.com/search%3Fq%3Dblocked%26oq%3Dblocked%26hl%3Den%26gl%3Dus%26sourceid%3Dchrome%26ie%3DUTF-8&hl=en&q=EgQyTvRhGNG5mOIFIhkA8aeDS7SXjlGndA50dtMVc_Y3HaUq6drtMgFy
+      #res = requests.get('https://google.com/search?q=internal%20error&q=EgQyTvRhGN&usasourc=chrome&ved=xUTF-8')
+      res = requests.get('https://www.google.com/sorry/index?continue=https://www.google.com/search%3Fq%3Dblocked%26oq%3Dblocked%26hl%3Den%26gl%3Dus%26sourceid%3Dchrome%26ie%3DUTF-8&hl=en')
+    elif (q == '400'):
+      res = requests.get('https://google.com/sorry?q=&oq=&hl=en&gl=us&sourceid=chrome&ie=UTF-8')
+    else:
+      res = requests.get('https://google.com/search?q=' +q+ '&oq='+q+'&hl=en&gl=us&sourceid=chrome&ie=UTF-8')
+    #res.raise_for_status()  # not in production
+    if (res.status_code >= 400) and (res.status_code < 500):
+      return('<html><head></head><body><h2>ERROR Returned '+str(res.status_code)+
+        '</h2><p>'+res.text+'<br></body></html>')
+
+    if (res.status_code >= 500) and (res.status_code < 600):
+      return('<html><head></head><body><h2>Internal Error Returned '+str(res.status_code)+
+        '</h2><p>'+res.text+'<br></body></html>')
 
 #   print some html reponse information
     if (verbose > 0):
