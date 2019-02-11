0e17f8472bdabd8d0fafee5831dffdc0b2a805eb
mkobar <mike@exlorev.com>
2019-02-11 15:00:49 -0500

    wsgi run file

---
+++ iSearchWsApi/wsgi.py
@@ -0,0 +1,11 @@
+# run this with gunicorn:
+#  gunicorn 'iSearchWsApi.wsgi'
+#
+# from
+# https://stackoverflow.com/questions/25319690/how-do-i-run-a-flask-app-in-gunicorn-if-i-used-the-application-factory-pattern
+#
+
+from app import create_app
+
+app = create_app('config.settings_production')
+
