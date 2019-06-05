2d61793a271706e07cb5e3bc92016a292183704b
mkobar <mike@exlorev.com>
2019-06-04 20:10:13 -0400

    added serpapi to api

--- iSearchWsApi/blueprints/api/api.py
+++ iSearchWsApi/blueprints/api/api.py
@@ -117,9 +117,10 @@ def googleApi():
     abstractElems = soup.select(".st")  # osearch snippets
     relatedSearches = soup.select(".aw5cc a")
     #   relatedQuestions = soup.select('.st span')
+    result_count = "About 1 results"
     for resultStats in soup.find_all("div", "sd"):
         result_count = resultStats.contents
-    #     print("s")
+        print("resultStats")
 
     #   for titleElems in soup.find_all("div", "r"):
     titleElems = soup.select(".r a")
@@ -142,8 +143,9 @@ def googleApi():
             print(*relatedSearches, sep="\n")
 
     #    print(*resultStats, sep = "\n")
-    #    total_results = int(resultStats[0])
+    #total_results = int(resultStats[0])
     total_results = result_count
+    #total_results = 0
 
     if verbose > 3:
         print("\n\ntotal_results")
@@ -159,7 +161,7 @@ def googleApi():
         print(data1)
 
     data1["search_parameters"]["q"] = q
-    data1["search_information"]["total_results"] = total_results[0]
+    #data1["search_information"]["total_results"] = total_results[0]
     # "organic_results": []
     for x in range(len(titleElems)):
         position = x
@@ -217,3 +219,125 @@ def bingApi():
 # multiple engines
 def multipleEnginesApi():
     return '{"message": "ERROR: not yet supported"}'
+
+
+@api.route("/serpapi")
+def serpApi():
+    # from
+    # https://automatetheboringstuff.com/chapter11/
+    q = request.args.get("q")  # not requests
+    mock = request.args.get("mock")  # not requests
+    blocked = request.args.get("blocked")  # not requests
+    print("q = " + q)
+    if mock:
+        print("mock = " + mock)
+    if blocked:
+        print("blocked = " + blocked)
+        return '{"message": "ERROR: we have been BLOCKED"}'
+
+    print("SerpAPIing...")  # display text while downloading the Google page
+
+    if mock:
+        if q == "kittens":
+            return jsonify(mockdata.mockDataKittens)
+        elif q == "cats":
+            return jsonify(mockdata.mockDataCats)
+        elif q == "cars":
+            return jsonify(mockdata.mockDataCars)
+        else:
+            return '{"message": "mocked"}'
+    # if (mock == 0):
+    if q == "500":
+        # 503 Server Error: Service Unavailable for url:
+        #   https://www.google.com/sorry/index?continue=
+        #   https://www.google.com/search%3Fq%3Dblocked%26oq%3D
+        #   blocked%26hl%3Den%26gl%3Dus%26sourceid%3D
+        #   chrome%26ie%3DUTF-8&hl=en&
+        #   q=EgQyTvRhGNG5mOIFIhkA8aeDS7SXjlGndA50dtMVc_Y3HaUq6drtMgFy
+        # res = requests.get('https://google.com/search?q=internal%20error&q=EgQyTvRhGN&usasourc=chrome&ved=xUTF-8')
+        res = requests.get(
+            "https://www.google.com/sorry/index?continue="
+            "https://www.google.com/search%3Fq%3Dblocked%26oq%3D"
+            "blocked%26hl%3Den%26gl%3Dus%26sourceid%3Dchrome%26ie%3DUTF-8&hl=en"
+        )
+    elif q == "400":
+        res = requests.get(
+            "https://google.com/sorry?q=&oq=&hl=en&gl=us&sourceid=chrome&ie=UTF-8"
+        )
+    else:
+        # res = requests.get('http://google.com/search?q=' + q)
+        # $ curl --get https://serpapi.com/search \
+        # -d q="Coffee" \
+        # -d hl="en" \
+        # -d gl="us" \
+        # -d google_domain="google.com" \
+        # -d api_key="194a4b22789db07b2e0957b87615316d0f0045918a9dd4b5f8e8162b4020da43" 
+        res = requests.get(
+            "https://serpapi.com/search.json?q="
+            + q
+            + "&hl=en&gl=us&google_domain=google.com"
+            + "&api_key=194a4b22789db07b2e0957b87615316d0f0045918a9dd4b5f8e8162b4020da43"
+        )
+        print(res)
+        #print(*res, sep="\n")
+        print(*res)
+        #print(*res.organic_results)
+        #print(*res.related_searches)
+    # res.raise_for_status() # not in production
+    if (res.status_code >= 400) and (res.status_code < 500):
+        print("Client ERROR Returned " + str(res.status_code))
+        # return(res.text)
+        return '{"message": "Client ERROR: ' + str(res.status_code) + '"}'
+
+    if (res.status_code >= 500) and (res.status_code < 600):
+        print("Server ERROR Returned " + str(res.status_code))
+        if "blocked" in res.text:
+            print("we've been blocked")
+            return '{"message": "ERROR: we have been BLOCKED"}'
+        # return(res.text)
+        return '{"message": "Server ERROR: ' + str(res.status_code) + '"}'
+
+    #   print some html reponse information
+    if verbose > 0:
+        print("status = " + str(res.status_code))
+        print(res.headers.get("content-type", "unknown"))
+
+    # Retrieve top search result links.
+    data = res.json()
+
+    print (data)
+
+    if data["search_metadata"]:
+        print("\n\nsearchMetadata")
+        print(data["search_metadata"], sep="\n")
+        
+        print(data["search_information"])
+        print(data["search_information"]["total_results"])
+        #print(data["search_information.total_results"])
+        result_count = data["search_information"]["total_results"]
+
+    if data["organic_results"]:
+        if verbose > 3:
+            print("\n\norganicResults")
+            print(data["organic_results"], sep="\n")
+
+        #print("\n\nabstractElems")
+        #print(res.organic_results)
+        #print(*abstractElems, sep="\n")
+
+    if data["related_searches"]:
+        if verbose > 3:
+            print("\n\nrelatedSearches")
+            print(data["related_searches"], sep="\n")
+
+    #    print(*resultStats, sep = "\n")
+    #total_results = int(resultStats[0])
+    total_results = result_count
+
+    if verbose > 3:
+        print("\n\ntotal_results")
+        print(total_results)
+
+    return jsonify(data)
+    # return ('{"message": "ERROR: not yet supported"}')
+
